8cf52e1f575390c5e3217a7c187930d93853a49a
JIexa <msleha100@gmail.com>
2021-07-31 15:22:47 +0300

    Тест @RequestParam

--- src/main/java/ru/java/web/controllers/FirstController.java
+++ src/main/java/ru/java/web/controllers/FirstController.java
@@ -3,13 +3,20 @@ package ru.java.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surName) {
+
+        System.out.println("Hello " + name + " " + surName);
+
         return "first/hello";
     }
 
