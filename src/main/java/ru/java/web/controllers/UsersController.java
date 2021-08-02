debb6ab7720e87ee7be6e1507e296f862d097e5b
JIexa <msleha100@gmail.com>
2021-08-02 11:27:27 +0300

    Настроен UserController и представления

--- src/main/java/ru/java/web/controllers/UsersController.java
+++ src/main/java/ru/java/web/controllers/UsersController.java
@@ -2,8 +2,8 @@ package ru.java.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.java.web.models.User;
 import ru.java.web.service.UserServiceImp;
 
 
@@ -23,5 +23,39 @@ public class UsersController {
         return "users/index";
     }
 
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userServiceImp.show(id));
+        return "users/show";
+    }
 
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "users/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("user") User user) {
+        userServiceImp.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userServiceImp.show(id));
+        return "users/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userServiceImp.update(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userServiceImp.delete(id);
+        return "redirect:/users";
+    }
 }
